ecb6c154f6a2d1746440befebab9c01eed400be4
mfigura61 <mikroial@yandex.ru>
2020-08-07 22:27:04 +0000

    script

--- raid.sh
+++ raid.sh
@@ -1,4 +1,4 @@
-#!/bin/bash           
+!/bin/bash           
 
             mdadm --zero-superblock --force /dev/sd{b,c,d,e,f,g}
             mdadm --create --verbose --force /dev/md0 -l 10 -n 6 /dev/sd{b,c,d,e,f,g}
